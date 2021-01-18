16b5648644d55b45f3783b6f6be436d0d5548e59
jonathandennis <4jondennis@gmail.com>
2021-01-18 13:49:17 +0100

    added detail modal functionality

--- src/components/Stone/Stone.js
+++ src/components/Stone/Stone.js
@@ -1,19 +1,30 @@
 import React from 'react'
+import Modal from "./Modal"
+import useModal from './useModal'
 
 import './Stone.css'
+import './Modal.css'
 
 const Stone = ({ stone }) => {
 //console.log('stone in Stone: ', stone)
+    const {isShowing, toggle} = useModal()
 
     return (
-      <figure className="stone__card">
-          <img className="stone__img"
-              src={`${process.env.PUBLIC_URL}/${stone.image}`}
-              alt={stone.name}
-          />
-          <h3 className="stone__card-head">{stone.name}</h3>
-          <p className="stone__card-type">{stone.type}</p>
-      </figure>
+      <div>
+        <figure className="stone__card" onClick={toggle}>
+            <img className="stone__img"
+                src={`${process.env.PUBLIC_URL}/${stone.image}`}
+                alt={stone.name}
+            />
+            <h3 className="stone__card-head">{stone.name}</h3>
+            <p className="stone__card-type">{stone.type}</p>
+        </figure>
+        <Modal
+          stone={stone}
+          isShowing={isShowing}
+          hide={toggle}
+        />
+      </div>
     )
 }
 
