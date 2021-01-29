e596186dfdfa98109da4ac1bdacb8576a010f284
jonathandennis <4jondennis@gmail.com>
2021-01-29 13:53:14 +0100

    Initial backend setup

--- Client/src/services/stones.js
+++ Client/src/services/stones.js
@@ -4,8 +4,6 @@ const baseUrl = '/api/stones'
 const promise = axios.get('http://localhost:3001/stones')
 console.log(promise)
 
-const promise2 = axios.get('http://localhost:3001/foobar')
-console.log(promise2)
 
 const getAll = () => {
     const request = axios.get(baseUrl)
